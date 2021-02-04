fdee1aff15a96898ba7e0a59fc496e9b93e69cff
Dmitry Rychkov <dmitry.rychkov@gmail.com>
2021-02-04 14:48:22 +0300

    first release

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
+let state = {
+  recent: null,
+  active: null
+}
+
+const update = (nextState) => {
+  state = {...state, ...nextState};
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  const activeTab = tabs[0].id;
+  update({active: activeTab});
+  chrome.tabs.onActivated.addListener((activeInfo) => {
+    const newTab = activeInfo.tabId;
+    update({recent: state.active, active: newTab})
+  });
+});
+
 chrome.commands.onCommand.addListener((command) => {
-  console.log('Command:', command);
+  if (command !== 'switch-tab') {
+    return;
+  }
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    const activeTab = tabs[0]?.id;
+    if (!activeTab) {
+      return;
+    }
+    chrome.tabs.update(state.recent, {active: true});
+  });
 });
